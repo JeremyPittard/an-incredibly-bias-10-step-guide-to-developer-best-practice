c7940bde8c0e7f2b22a4067509ccc168b5e45996
Jeremy Pittard <jeremy@jpittard.net>
2023-08-03 14:56:36 +0800

    initial

---
+++ src/slides.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Presentation, Slide } from '@components'
+</script>
+
+<Presentation>
+	<Slide>
+		<h1 class="font-bold bg-neon-pink text-neon-pink-900 text-2xl py-3 px-4">An incredibly bias 10 step guide to developer best practice</h1>
+	</Slide>
+</Presentation>
