925f42185d0a2c2c7771ab53548eeac81db01f6e
Jeremy Pittard <jeremy@jpittard.net>
2023-08-15 21:40:39 +0800

    more slides

--- src/slides/framework.svelte
+++ src/slides/framework.svelte
@@ -6,3 +6,17 @@
 <Slide class="text-left" image={bg} transition="none">
 	<h1 class="sr-only">08. Use a framework</h1>
 </Slide>
+<Slide class="text-left" transition="none">
+	<h2 class="font-bold mb-6">How come?</h2>
+	<ul>
+		<li class="fragment">Efficiency.</li>
+		<li class="fragment">Consistency.</li>
+		<li class="fragment">Scalability.</li>
+		<li class="fragment">Security.</li>
+		<li class="fragment">Maintenance.</li>
+		<li class="fragment">Ecosystem.</li>
+		<li class="fragment">Future-Proofing.</li>
+		<li class="fragment">Testing and Debugging.</li>
+		<li class="fragment">Accessibility.</li>
+	</ul>
+</Slide>
--- src/slides/test.svelte
+++ src/slides/test.svelte
@@ -6,3 +6,20 @@
 <Slide class="text-left" image={bg} transition="none">
 	<h1 class="sr-only">10. Test.</h1>
 </Slide>
+<Slide class="text-left" transition="none">
+	<h2 class="font-bold mb-6">Twelve Testing Tips</h2>
+	<ul>
+		<li class="fragment">Start Early and Continuously.</li>
+		<li class="fragment">Test Automation.</li>
+		<li class="fragment">Test Coverage.</li>
+		<li class="fragment">Unit, Integration, and End-to-End Testing.</li>
+		<li class="fragment">Test-Driven Development.</li>
+		<li class="fragment">Regression Testing.</li>
+		<li class="fragment">Exploratory Testing.</li>
+		<li class="fragment">Mocking and Stubbing.</li>
+		<li class="fragment">Continuous Integration and Continuous Deploymen.</li>
+		<li class="fragment">Performance and Security Testing.</li>
+		<li class="fragment">Collaboration.</li>
+		<li class="fragment">Test Data Management.</li>
+	</ul>
+</Slide>
